a7ce5a9b9784d3480a86667dcd7f97ef03694854
ErikOS <errikos.gioussef@gmail.com>
2024-07-06 14:40:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+
+var imageNumber1 = "dice" + randomNumber1 +".png";
+
+var diceImage1 = "./images/"+ imageNumber1;
+
+var randomNumber2 = Math.ceil(Math.random()*6);
+
+var imageNumber2 = "dice" + randomNumber2 +".png";
+
+var diceImage2 = "./images/"+ imageNumber2;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",diceImage1);
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",diceImage2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h2").innerHTML = "Player1 wins";
+}
+
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h2").innerHTML = "Player2 wins";    
+}
+
+else {
+    document.querySelector("h2").innerHTML = "Draw - Please try again";
+}
